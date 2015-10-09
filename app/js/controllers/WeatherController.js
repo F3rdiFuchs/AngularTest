bb774f38bdcbe1a6e164cb7f24ff6ebd5b5879e0
F3rdiFuchs <f.bau3r@gmail.com>
2015-10-09 08:21:11 +0200

    ..

--- app/js/controllers/WeatherController.js
+++ app/js/controllers/WeatherController.js
@@ -3,6 +3,17 @@ app.controller('WeatherController', ['$scope', 'weatherFactory', function($scope
     $scope.weatherEf;
     getWeatherErfurt();
 
+    $scope.toggle = function() {
+        var city = $scope.iCity;
+        weatherFactory.getCityWeather(city)
+            .success(function(city){
+                $scope.weatherS = city;
+            })
+            .error(function (error) {
+                $scope.status = 'Unable to load weather data: ' + error.message;
+            })
+
+    }
     function getWeatherErfurt() {
         var city = 'erfurt';
         weatherFactory.getCityWeather('erfurt')
--- app/js/services/WeatherService.js
+++ app/js/services/WeatherService.js
@@ -1,5 +1,5 @@
 app.factory('weatherFactory', ['$http', function($http) {
-    var urlBase = 'http://www.api.openweathermap.org/data/2.5/weather?q=';
+    var urlBase = 'http://api.openweathermap.org/data/2.5/weather?q=';
     var dataFactory = {};
 
     dataFactory.getCityWeather = function(city) {
