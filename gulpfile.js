4e7d472060c060bd212d1ba38649d0c5f82cf4d8
F3rdiFuchs <f.bau3r@gmail.com>
2015-10-22 16:32:44 +0200

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
-var gulp = require('gulp');
-var changed = require('gulp-imagemin');
+var gulp = require('gulp'), inject = require('gulp-inject');
 
 gulp.task('default', function() {
     // place code for your default task here
 });
-gulp.task('images', function() {
+
+gulp.task('updateIndex', function() {
+    var target = gulp.src('./app/index.html');
+    var sources = gulp.src([
+        './app/js/**/*.js',
+            './app/css/**/*.css',
+            './app/',
+            './bower_components/**/*.js',
+            './bower_components/**/*.css'
+        ],
+        {read:false})
+
+    return target.pipe(inject(sources))
+        .pipe(gulp.dest('./app'))
 
 });
